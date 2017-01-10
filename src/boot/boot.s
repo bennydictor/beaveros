0c7b0e259233aa32b3582db268b783dea9e5a8b8
zaxdo <zaxdo@users.noreply.github.com>
2017-01-10 11:56:12 +0300

    get/setcr

--- src/boot/boot.s
+++ src/boot/boot.s
@@ -1,9 +1,59 @@
 .section    .text
 .globl      start
-.type       start,                          @function
+.type       start,                  @function
 
-start:      cli
-            mov     $0x2f592f412f4b2f4f,    %rax
-            mov     %rax,                   0xb8000
-.Lhlt:      hlt
-            jmp     .Lhlt
+start:
+    cli
+    mov     $0x2f592f412f4b2f4f,    %rax
+    mov     %rax,                   0xb8000
+.Lhlt:
+    hlt
+    jmp     .Lhlt
+
+.globl      get_cr3
+.type       get_cr3,                @function
+
+get_crx:
+    jmp     *$.Ljmptable(,%rdi,8)
+.LjumpTable
+    .quad   $.Lcr0
+    .quad   0
+    .quad   $.Lcr2
+    .quad   $.Lcr3
+    .quad   $.Lcr4
+.Lcr0:
+    mov     %cr0,                   %rax
+    ret
+.Lcr2:
+    mov     %cr2,                   %rax
+    ret
+.Lcr3:
+    mov     %cr3,                   %rax
+    ret
+.Lcr4:
+    mov     %cr4,                   %rax
+    ret
+
+
+
+set_crx:
+    jmp     *$.Ljmptable(,%rdi,8)
+.LjumpTable
+    .quad   $.Lcr0
+    .quad   0
+    .quad   $.Lcr2
+    .quad   $.Lcr3
+    .quad   $.Lcr4
+.Lcr0:
+    mov     %rsi,                   %cr0
+    ret
+.Lcr2:
+    mov     %rsi,                   %cr2
+    ret
+.Lcr3:
+    mov     %rsi,                   %cr3
+    ret
+.Lcr4:
+    mov     %rsi,                   %cr4
+    ret
+
