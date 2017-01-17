1d1c47822a87c4151bf1955583409dabd2ffbc02
benny <sorokinmisha007@gmail.com>
2017-01-18 00:33:53 +0300

    Fix coding style

--- src/include/cpuid.h
+++ src/include/cpuid.h
@@ -4,8 +4,8 @@
 #include <stdint.h>
 
 static inline void cpuid(uint32_t code, uint32_t registers[4]) {
-    __asm__ __volatile__("cpuid" : "=a"(registers[0]), "=b"(registers[1]),
-            "=c"(registers[2]), "=d"(registers[3]) : "a"(code));
+    asm volatile ("cpuid":"=a"(registers[0]), "=b"(registers[1]),
+            "=c"(registers[2]), "=d"(registers[3]):"a"(code));
 }
 
 #endif /* BEAVER_CPUID_H */
