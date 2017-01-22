9057b03652f8eba39721ba8a35be25400b2e52c1
benny <sorokinmisha007@gmail.com>
2017-01-22 17:54:00 +0300

    Working APIC

--- src/isr/apic.c
+++ src/isr/apic.c
@@ -9,6 +9,7 @@
 extern void *_apic_page;
 static uint32_t *const apic_page = (uint32_t *) &_apic_page;
 
+/* TODO: spurious isr */
 static void spurious_isr(interrupt_frame_t frame __attribute__ ((unused))) {
     printf("spurious!!\n");
 }
@@ -20,16 +21,6 @@ void apic_init(void) {
     if (!(edx & CPUID_1_EDX_APIC_BIT)) {
         PANIC("no APIC available");
     }
-
-    uint64_t msr = rdmsr(APIC_BASE_MSR);
-    if (!(msr & APIC_BASE_ENABLE_BIT)) {
-        PANIC("APIC is not enabled\nReboot and try again");
-    }
-
-    uint64_t apic_phys_page = msr & PAGE_ADDR_BITS;
-    printf("Mapping %pp to %pv\n", (void *) apic_phys_page, apic_page);
-    map_page(apic_page, (void *) apic_phys_page,
-            PAGE_P_BIT | PAGE_RW_BIT | PAGE_G_BIT);
 
     /* Disable PIC 8259 */
     for (uint8_t pic_irq = 0; pic_irq < 8; ++pic_irq) {
@@ -43,6 +34,12 @@ void apic_init(void) {
         outb(PORT_PIC8259_SLAVE_DATA, val);
     }
 
+    /* Map APIC's page */
+    uint64_t msr = rdmsr(APIC_BASE_MSR);
+    uint64_t apic_phys_page = msr & PAGE_ADDR_BITS;
+    map_page(apic_page, (void *) apic_phys_page,
+            PAGE_P_BIT | PAGE_RW_BIT | PAGE_G_BIT);
+
     /* Reset APIC */
     wrapic(APIC_DFR_REGISTER, APIC_DFR_CLUSTER);
     uint32_t ldr = rdapic(APIC_LDR_REGISTER);
@@ -55,19 +52,13 @@ void apic_init(void) {
     wrapic(APIC_LINT1_LVT_REGISTER, APIC_LINT1_LVT_DISABLE);
     wrapic(APIC_TPR_REGISTER, 0);
 
-    /* Enable APIC */
-    wrmsr(APIC_BASE_MSR, msr);
-
-    /* Set spurious-interrupt handler */
     install_isr(spurious_isr, SPURIOUS_VECTOR);
-    wrapic(APIC_SPURIOUS_LVT_REGISTER,
-            SPURIOUS_VECTOR | APIC_SPURIOUS_LVT_ENABLE);
 }
 
 void wrapic(uint16_t number, uint32_t value) {
-    apic_page[number] = value;
+    apic_page[number / sizeof(uint32_t)] = value;
 }
 
 uint32_t rdapic(uint16_t number) {
-    return apic_page[number];
+    return apic_page[number / sizeof(uint32_t)];
 }
--- src/isr/timer.c
+++ src/isr/timer.c
@@ -4,14 +4,14 @@
 #include <terminate.h>
 
 static void timer_isr(interrupt_frame_t frame __attribute__ ((unused))) {
-    printf("hOI!!!!!! I'm tIMER!!\n"); /* FIXME: timer_isr isn't called */
+    printf("hOI!!!!!! I'm tIMER!!\n");
     wrapic(APIC_EOI_REGISTER, 0);
 }
 
 void timer_init(void) {
     install_isr(timer_isr, TMR_VECTOR);
 
-    wrapic(APIC_TMR_INITCNT_REGISTER, 1000000);
+    wrapic(APIC_TMR_INITCNT_REGISTER, 10000000);
     wrapic(APIC_TMR_LVT_REGISTER, TMR_VECTOR | APIC_TMR_LVT_PERIODIC);
     wrapic(APIC_TMR_DIV_REGISTER, APIC_TMR_DIV_DIV16);
 }
